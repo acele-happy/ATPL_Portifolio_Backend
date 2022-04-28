eb3b3713c194d680a6e249a37d2a8f166d5d001c
acele-happy <78531760+acele-happy@users.noreply.github.com>
2022-04-28 07:59:40 +0200

    f-documentation

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ import dotenv from 'dotenv'
 //Swagger
 import swaggerUi from 'swagger-ui-express'
 
+//to use require in es6 you need to import it!
+
+import {createRequire} from 'module'
+const require = createRequire(import.meta.url)
+const swaggerDocument = require('./swagger.json')
+
 
 dotenv.config()
 const PORT = process.env.PORT
@@ -16,7 +22,7 @@ app.use(json())
 app.use(urlencoded({extended:true}))
 app.use(routes)
 app.use(cors())
-// app.use('/documentation',swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/documentation',swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 const server = app.listen(PORT,()=>console.log("running on port "+ PORT))
