dfb2f19bacc28a2b0ecdebdeb78fa1a20e9830da
acele-happy <78531760+acele-happy@users.noreply.github.com>
2022-05-06 16:52:14 +0200

    uptest

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,16 @@
-const jwt =require('jsonwebtoken')
-const {verify} = jwt
+const jwt = require("jsonwebtoken");
+const { verify } = jwt;
 
-module.exports= function(req,res,next){
-    const token = req.header('Authorization')
-    if(!token){
-        return res.status(401).send("Access Denied! Login first")
-    }
-    try{
-        const tokenArray = token.split(" ");
-        let user = verify(tokenArray[1],(process.env.JWT_SECRET))
-        next()
-    }catch(e){
-        res.status(400).send("Invalid token")
-    }
-
-}
+module.exports = function (req, res, next) {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).send("Access Denied! Login first");
+  }
+  try{
+    const tokenArray = token.split(" ");
+    let user = verify(tokenArray[1], process.env.JWT_SECRET);
+    next();
+  }catch(e){
+    return res.status(400).send("Invalid token");
+  }
+};
